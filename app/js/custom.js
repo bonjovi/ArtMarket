262c8b13a4c482225032cbcc5eb377cb7852e0f4
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2018-05-21 20:44:37 +0300

    Added last changes for main page media queries

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,4 +1,22 @@
-Array.prototype.forEach.call(document.querySelectorAll('[data-ripple]'), function(element){
-        // find all elements and attach effect
-        new RippleEffect(element); // element is instance of javascript element node
-    });
+$(function() {
+	$('.footer__toggler i').on('click', function() {
+		$('.footer__left .footer__section').slideToggle();
+	});
+
+	
+
+	$('.header__toggler i').on('click', function() {
+		if($('.topmenu__wrapper').hasClass('topmenu__wrapper_hidden')) {
+			$('.main, .footer').hide();
+		} else {
+			$('.main, .footer').show();
+		}
+		$('.topmenu__wrapper').toggleClass('topmenu__wrapper_hidden');
+		$('.header__toggler i').toggleClass('active');
+		$('.header__toggler i').text('menu');
+		$('.header__toggler i.active').text('close');
+	});
+
+	
+
+});
