2522b17aa41bc97531fc7dbf4f228f6c7b5968bb
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2018-05-24 22:20:38 +0300

    Added filter to main page

--- app/js/custom.js
+++ app/js/custom.js
@@ -17,6 +17,81 @@ $(function() {
 		$('.header__toggler i.active').text('close');
 	});
 
+	// СЛАЙДЕР ЦЕН В ФИЛЬТРЕ
+	var slider = document.getElementById('slider');
+
+	noUiSlider.create(slider, {
+		start: [20000, 80000],
+		connect: true,
+		range: {
+			'min': 0,
+			'max': 100000
+		},
+		format: wNumb({
+            decimals: 0,
+            thousand: ' ',
+            postfix: ' руб.',
+        })
+	});
+
+	var marginMin = document.getElementById('slider-margin-value-min'),
+		marginMax = document.getElementById('slider-margin-value-max');
+
+	slider.noUiSlider.on('update', function ( values, handle ) {
+		if ( handle ) {
+			marginMax.innerHTML = values[handle];
+		} else {
+			marginMin.innerHTML = values[handle];
+		}
+	});
+	// end of СЛАЙДЕР ЦЕН В ФИЛЬТРЕ
+
+	$('.filter__title').on('click', function() {
+		$(this).next('.filter__sectioncontent').slideToggle();
+		var toggler = $(this).find('i');
+
+		if(toggler.text() == 'remove') {
+			toggler.text('add');
+		} else {
+			toggler.text('remove');
+		}
+	});
+
+	$('.filterToggler').on('click', function(e) {
+		e.preventDefault();
+		if($(this).text() == 'ПОКАЗАТЬ ФИЛЬТРЫ') {
+			$(this).text('СКРЫТЬ ФИЛЬТРЫ')
+		} else {
+			$(this).text('ПОКАЗАТЬ ФИЛЬТРЫ')
+		}
+
+		if($('.filter').hasClass('filter_uncollapsed')) {
+			$('.filter > *').hide();
+			$('.filter').css('padding', '0').css('margin-right', '0');
+		} else {
+			$('.filter > *').show();
+			$('.filter').css('padding', '40px 20px 40px 40px').css('margin-right', '20px');
+		}
+
+		$('.filter').toggleClass('filter_uncollapsed');
+
+		changeCardMargin();
+
+	});
+
+	$(window).resize(function(){
+	  	changeCardMargin();
+	});
 	
+	function changeCardMargin() {
+		setTimeout(function() {
+			console.log($('.cards').width());
+			if($('.cards').width() < 1160 && $('.cards').width() > 640) {
+				$('.cards:first-child .card:nth-child(3n)').css('margin-right','15px');
+			} else {
+				$('.cards:first-child .card:nth-child(3n)').css('margin-right','0');
+			}
+		}, 500);
+	}
 
 });
